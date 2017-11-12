8164fc100167943f64f08b15f933c9071eb6b480
Michele Semeghini <mhl.semeghini@gmail.com>
2017-11-12 13:02:10 +0100

    Added new command

---
+++ MSE_Toolbox/Commands/MSE_TestCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using Rhino.UI;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace MSE_Toolbox
+{
+    public class MSE_TestCommand : Rhino.Commands.Command
+    {
+        
+        public override string EnglishName
+        {
+            get { return "MSE_TestCommand"; }
+        }
+
+        protected override Result RunCommand(Rhino.RhinoDoc doc, RunMode mode)
+        {
+            // TODO: start here modifying the behaviour of your command.
+
+
+
+            return Result.Success;
+        }
+    }
+}
